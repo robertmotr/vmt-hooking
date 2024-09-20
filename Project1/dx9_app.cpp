5a58d77cc9535f90a4c42ee1a418ba6c19e6b181
Robert Motrogeanu <robert.motrogeanu@mail.utoronto.ca>
2024-09-20 17:06:45 -0400

    finished dx9 app example

--- Project1/dx9_app.cpp
+++ Project1/dx9_app.cpp
@@ -1,12 +1,7 @@
 #include <windows.h>
 #include <d3d9.h>
 #include <d3dx9.h>
-#include <detours.h>
-#include <imgui.h>
-#include <imgui_impl_dx9.h>
-#include <imgui_impl_win32.h>
 
-// link required DX9 library (todo: find out exactly why? i hate development on windows)
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
 
@@ -14,7 +9,6 @@ LPDIRECT3D9 d3d = nullptr;
 LPDIRECT3DDEVICE9 d3dDevice = nullptr;
 LPD3DXFONT font = nullptr;
 
-// window procedure function
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
     case WM_DESTROY:
@@ -41,24 +35,19 @@ bool InitD3D(HWND hwnd) {
         return false;
     }
 
-    // Create a font
-    D3DXCreateFont(d3dDevice, 24, 0, FW_BOLD, 1, FALSE, DEFAULT_CHARSET,
-        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
-        DEFAULT_FANTASY, L"Arial", &font);
+	D3DXCreateFont(d3dDevice, 30, 0, FW_BOLD, 1, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Arial", &font);    
 
     return true;
 }
 
-// Render frame
 void Render() {
     if (d3dDevice) {
         d3dDevice->Clear(0, nullptr, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
         d3dDevice->BeginScene();
 
-        // Set the font and draw the text
         RECT rect;
-        SetRect(&rect, 100, 100, 0, 0); // Positioning the text
-        font->DrawText(NULL, L"Hello, World!", -1, &rect, DT_NOCLIP, D3DCOLOR_XRGB(255, 255, 255));
+        SetRect(&rect, 0, 100, 0, 0); // Positioning the text
+        font->DrawText(NULL, L"Original DX9 app, nothing has been injected and hooked yet.", -1, &rect, DT_NOCLIP, D3DCOLOR_XRGB(255, 255, 255));
 
         d3dDevice->EndScene();
         d3dDevice->Present(nullptr, nullptr, nullptr, nullptr);
@@ -67,7 +56,6 @@ void Render() {
 
 // entry
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Register window class
     const wchar_t CLASS_NAME[] = L"DX9WindowClass"; // Changed to wide character string
 
     WNDCLASS wc = {};
